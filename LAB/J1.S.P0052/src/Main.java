c8d373464b78e20e9a9d9eff20300b699bdde79f
CnydoX <oraclevn8@gmail.com>
2022-02-07 18:34:38 +0700

    a

--- LAB/J1.S.P0052/src/Main.java
+++ LAB/J1.S.P0052/src/Main.java
@@ -1,15 +1,14 @@
 
 import java.util.ArrayList;
 
-
 public class Main {
 
     public static void main(String[] args) {
         ManageEastAsiaCountries m = new ManageEastAsiaCountries();
-        ArrayList<EastAsiaCountries> countriesList = new ArrayList<>();
+        ArrayList<EastAsiaCountries> list = new ArrayList<>();
         EastAsiaCountries newCountry = null;
         int choice;
-        do {//Return to main screen after each after each option
+        do {//Return to main screen after each after finishing the function
 
             //step 1. Display the menu
             Utility.displayMenu();
@@ -18,7 +17,7 @@ public class Main {
             //step 3. Perform function based on the selected option
             switch (choice) {
                 case 1://Add information of 11 countries
-                    newCountry = m.addCountryInformation(countriesList);
+                    newCountry = m.addCountryInformation(list);
                     break;
                 case 2://Display information of country user has just entered
                     try {
@@ -29,14 +28,16 @@ public class Main {
                     break;
                 case 3://Search information of country by user-entered name
                     try {
-                        Utility.displayArray(m.searchInformationByName(countriesList));
+                        Utility.displayArray(
+                                m.searchInformationByName(list));
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
                     break;
                 case 4://Display country by ascending order of name
                     try {
-                        Utility.displayArray(m.sortInformationByAscendingOrder(countriesList));
+                        Utility.displayArray(
+                            m.sortInformationByAscendingOrder(list));
                     } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
