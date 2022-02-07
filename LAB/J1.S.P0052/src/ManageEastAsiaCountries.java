9569f4ceff99ab3c1bc078301b25fc970ae97566
CnydoX <oraclevn8@gmail.com>
2022-02-07 10:45:08 +0700

    l

--- LAB/J1.S.P0052/src/ManageEastAsiaCountries.java
+++ LAB/J1.S.P0052/src/ManageEastAsiaCountries.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author ADMIN
