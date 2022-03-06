fa1e52099b026bf6b0efac68116ff04debf741cc
AN THANH LONG <oraclevn8@gmail.com>
2022-03-06 23:37:49 +0700

    add comment

--- LAB/J1.S.P0074/src/Main.java
+++ LAB/J1.S.P0074/src/Main.java
@@ -3,23 +3,30 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        //step 1: display menu
-        calculator.displayMenu();
-        //step 2: ask users to select an option
-        int choice = calculator.selectOption("Your choice: ", 1, 4);
-        //step 3: perform function based on the selected option
-        switch (choice) {
-            case 1:
-                calculator.addMatrix();
-                break;
-            case 2:
-                calculator.subtractMatrix();
-                break;
-            case 3:
-                calculator.multiplyMatrix();
-                break;
-            case 4:
-                break;
-        }
+        //Return to main menu unless user choose exit
+        do {            
+            //step 1: display menu
+            calculator.displayMenu();
+            //step 2: ask users to select an option
+            int choice = calculator.selectOption("Your choice: ", 1, 4);
+            //step 3: perform function based on the selected option
+            switch (choice) {
+                //Addition
+                case 1:
+                    calculator.addMatrix();
+                    break;
+                //Subtraction
+                case 2:
+                    calculator.subtractMatrix();
+                    break;
+                //Multiplication
+                case 3:
+                    calculator.multiplyMatrix();
+                    break;
+                //Exit
+                case 4:
+                    System.exit(0);
+            }
+        } while (true);
     }
 }
